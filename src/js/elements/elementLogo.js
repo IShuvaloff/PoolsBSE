651b820b66cf988a62abf0a5eaa6bfc5c2846dcd
Ilya A. Shuvalov <i.a.shuvalov@gmail.com>
2023-07-29 18:44:38 +0300

    мелкие правки в анимациях и транзишнах

--- src/js/elements/elementLogo.js
+++ src/js/elements/elementLogo.js
@@ -9,6 +9,7 @@ function openPageMain() {
 
 export default function elementLogo(className) {
   const logo = el(`.icon-logo.${className}__logo`, 'BSE');
+  logo.tabIndex = '0';
   logo.addEventListener('click', openPageMain);
 
   return logo;
